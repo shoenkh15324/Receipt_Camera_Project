217139bcbd3c8c748c4a0c3222cc33a11f5d855d
shoenkh15324 <mok07333@gmail.com>
2024-02-23 20:30:38 +0900

    프린터 코드 수정

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 import camera
-<<<<<<< HEAD
 import printer
-=======
-import BLE
-#import printer
->>>>>>> f9df0a73d4789ed0ade3d731778a1df040d36f2d
 
 # 카메라 ON
 camera.capture() # bmp이미지 파일의 주소 전달
-
-# 블루투스 연결
-
 
 # 프린터 제어
 printer.print_image()
